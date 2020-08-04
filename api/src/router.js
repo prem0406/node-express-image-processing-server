9ce75c8125e05fe4200520827cae673f268adcd9
prem0406 <premk191@gmail.com>
2020-08-04 17:05:04 +0530

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
-router.get('photo-viewer', (request, response)=>{
+router.get('/photo-viewer', (request, response)=>{
     response.sendFile(photoPath);
 });
 
