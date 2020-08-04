ea2259d53b1fb0b5e561c44da243e703fc88a33b
prem0406 <premk191@gmail.com>
2020-08-04 23:13:01 +0530

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor')
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
@@ -31,10 +32,14 @@ const upload = multer({
     storage
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if(request.fileValidationError)   
      return response.status(400).json({ error: request.fileValidationError});
-    
+    try {
+        await imageProcessor(request.file.filename);
+    } catch (error) {
+        
+    }
 
     return response.status(201).json({success: true });
 });
