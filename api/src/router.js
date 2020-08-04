fc5afa5622aeca42266c8ce3dacaa2966643cd34
prem0406 <premk191@gmail.com>
2020-08-04 16:47:44 +0530

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -10,26 +10,26 @@ const storage = multer.diskStorage({
     filename: filename
 });
 
-function fileFilter(request, file, callback){
+const fileFilter = (request, file, callback) => {
     if(file.mimetype !== 'image/png'){
-        request.fileValidationError = 'Wrong File Type';
-        callback(null, false, new Error('Wrong File Type'));
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
     } else {
         callback(null, true);
     }
-}
+};
 
 const upload = multer({
     fileFilter,
     storage
 });
 
-router.post('/upload', upload.single('photo'), (request, response) =>{
+router.post('/upload', upload.single('photo'), (request, response) => {
     if(request.fileValidationError)   
      return response.status(400).json({ error: request.fileValidationError});
     
 
     return response.status(201).json({success: true });
-})
+});
 
 module.exports = router;
